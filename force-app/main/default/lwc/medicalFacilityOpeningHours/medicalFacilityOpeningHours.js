8ea4c816b0056f4cd8ebee22b6d470ab07074e0e
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2024-01-06 21:30:18 +0100

    add medical facility opening hours component

--- force-app/main/default/lwc/medicalFacilityOpeningHours/medicalFacilityOpeningHours.js
+++ force-app/main/default/lwc/medicalFacilityOpeningHours/medicalFacilityOpeningHours.js
@@ -1,3 +1,39 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api, wire } from "lwc";
+import { getRecord, getFieldValue } from "lightning/uiRecordApi";
 
-export default class MedicalFacilityOpeningHours extends LightningElement {}
+import MONDAY from "@salesforce/schema/Medical_Facility__c.Monday__c";
+import TUESDAY from "@salesforce/schema/Medical_Facility__c.Tuesday__c";
+import WEDNESDAY from "@salesforce/schema/Medical_Facility__c.Wednesday__c";
+import THURSDAY from "@salesforce/schema/Medical_Facility__c.Thursday__c";
+import FRIDAY from "@salesforce/schema/Medical_Facility__c.Friday__c";
+import SATURDAY from "@salesforce/schema/Medical_Facility__c.Saturday__c";
+import SUNDAY from "@salesforce/schema/Medical_Facility__c.Sunday__c";
+
+const FIELDS = [MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY];
+
+export default class MedicalFacilityOpeningHours extends LightningElement {
+  @api recordId;
+  @wire(getRecord, { recordId: "$recordId", fields: FIELDS }) facility;
+
+  get monday() {
+    return getFieldValue(this.facility.data, MONDAY);
+  }
+  get tuesday() {
+    return getFieldValue(this.facility.data, TUESDAY);
+  }
+  get wednesday() {
+    return getFieldValue(this.facility.data, WEDNESDAY);
+  }
+  get thursday() {
+    return getFieldValue(this.facility.data, THURSDAY);
+  }
+  get friday() {
+    return getFieldValue(this.facility.data, FRIDAY);
+  }
+  get saturday() {
+    return getFieldValue(this.facility.data, SATURDAY);
+  }
+  get sunday() {
+    return getFieldValue(this.facility.data, SUNDAY);
+  }
+}
