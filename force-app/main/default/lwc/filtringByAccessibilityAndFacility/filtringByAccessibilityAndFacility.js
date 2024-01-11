0fc3d2217d69459f36de57abf48ba1f70f33e21f
Jakub <240810@edu.p.lodz.pl>
2024-01-11 01:07:54 +0100

    added lwc for booking appointment

--- force-app/main/default/lwc/filtringByAccessibilityAndFacility/filtringByAccessibilityAndFacility.js
+++ force-app/main/default/lwc/filtringByAccessibilityAndFacility/filtringByAccessibilityAndFacility.js
@@ -1,15 +1,66 @@
 import { api, LightningElement, wire, track } from 'lwc';
-import getDoctors from '@salesforce/apex/AppointmentController.getAllDoctors';
+import getDoctors from '@salesforce/apex/AppointmentController.getAllDoctorsWorkingInCurrentFacility';
+import getSpecialization from '@salesforce/apex/AppointmentController.getAllSpecializationsFromDoctorsWorkingInAFacility';
+import getFacilities from '@salesforce/apex/AppointmentController.getAllFacilities';
 
 export default class FiltringByAccessibilityAndFacility extends LightningElement {
+    @api patientId;
+    
     @track doctors = [];
-    @track selectedDoctorId;
+    @track facilities = [];
+    // @track specializations = [];
+    picklistOptions = [
+        { label: "Online", value: "online" },
+        { label: "On-Site", value: "onSite" }
+    ];
 
-    @wire(getDoctors)
+    @track selectedDoctorId;
+    @track selectedFacilityId = null;
+    @track selectedSpecializationId = null;
+    @track selectedSpecializationLabel = null;
+    @track selectedPicklistValue;
+    @track specializations;
+
+
+    @wire(getFacilities)
+    wiredFacilities({ error, data }) {
+        if (data) {
+            this.facilities = data.map(facilitiy => ({
+                label: facilitiy.Name,
+                value: facilitiy.Id
+            }));
+        } else if (error) {
+            console.error('Błąd pobierania danych o placówkach', error);
+        }
+    }
+
+    // @wire(getSpecialization, { facilityId: "$selectedFacilityId" })
+    // wiredSpecialization({ error, data }) {
+    //     if (data) {
+    //         this.specializations = data.map(specialization => ({
+    //             label: specialization.Specialization__c,
+    //             value: specialization.Id
+    //         }));
+    //     } else if (error) {
+    //         console.error('Błąd pobierania specjalizacji', error);
+    //     }
+    // }
+
+    @wire(getSpecialization, { facilityId: "$selectedFacilityId" })
+    wiredSpecialization({ error, data }) {
+        if (data) {
+            this.specializations = Array.from(new Set(data.map(specialization => specialization.Specialization__c)));
+            console.log(this.specializations);
+        } else if (error) {
+            console.error('Błąd pobierania specjalizacji', error);
+        }
+    }
+    
+    @wire(getDoctors, { facilityId: "$selectedFacilityId", specialization: "$selectedSpecializationLabel" })
     wiredDoctors({ error, data }) {
         if (data) {
             this.doctors = data.map(doctor => ({
-                label: doctor.Name,
+                label: doctor.Name + " " + doctor.Last_Name__c,
                 value: doctor.Id
             }));
         } else if (error) {
@@ -17,12 +68,59 @@ export default class FiltringByAccessibilityAndFacility extends LightningElement
         }
     }
 
-    get doctorOptions() {
-        return this.doctors;
+    handleFacilityChange(event) {
+        this.selectedFacilityId = event.detail.value;
+        this.selectedSpecializationId = null;
+        this.selectedDoctorId = null;
+        this.doctors = [];
+    }
+
+    handleSpecializationChange(event) {
+        this.selectedSpecializationId = event.detail.value;
+        // this.selectedSpecializationLabel = ;
+        // console.log("label: " + this.specializations[this.selectedSpecializationId])
     }
 
     handleDoctorChange(event) {
         this.selectedDoctorId = event.detail.value;
-        console.log('Wybrano lekarza:', this.selectedDoctorId);
     }
+
+    handlePicklistChange(event) {
+        this.selectedPicklistValue = event.detail.value;
+    }
+
+    handleDateTimeChange(event) {
+        this.selectedDateTime = event.target.value;
+    }
+
+    handleAppointmentBooking() {
+
+    }
+
+    get facilitisOptions() {
+        return this.facilities;
+    }
+
+    get specializationOptions() {
+        return this.specializations;
+    }
+
+    get doctorOptions() {
+        return this.doctors;
+    }
+
+    get isSpecializationDisabled() {
+        return !this.selectedFacilityId;
+    }
+
+    get isDoctorDisabled() {
+        return !this.selectedSpecializationId || !this.selectedFacilityId;
+    }
+
+    get isBookDisabled() {
+        return  !this.selectedFacilityId ||
+                !this.selectedSpecializationId ||
+                !this.selectedDoctorId ||
+                !this.selectedDateTime;
+    }    
 }
